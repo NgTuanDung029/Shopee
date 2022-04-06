aec15465821bc8b4e9a464956affaf1e6bbac928
NgTuanDung029 <rivencyber@gmail.com>
2022-04-06 15:02:02 +0700

    sua loi button tang giam

--- assets/js/goods.js
+++ assets/js/goods.js
@@ -53,10 +53,12 @@ const increAndDecre = document.querySelector('.incre-and-decre');
 //btnAdd disable when increAndDecre is 9
 btnAdd.addEventListener('click', () => {
     increAndDecre.value = parseInt(increAndDecre.value) + 1;
+    console.log(increAndDecre.value);
 });
 
 btnSubtract.addEventListener('click', () => {   
     if (increAndDecre.value > 1){
         increAndDecre.value = parseInt(increAndDecre.value) - 1;
+        console.log(increAndDecre.value);
     }
 });
