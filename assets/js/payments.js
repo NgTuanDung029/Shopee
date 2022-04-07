9c7015e9421504c786fac291dfabf99cfad47c0c
viduc2811 <viduc28112002@gmail.com>
2022-04-08 00:32:11 +0700

    Hoan thien chuc nang trang Payments

--- assets/js/payments.js
+++ assets/js/payments.js
@@ -2,15 +2,38 @@ let count = 0;
 const btnAdd = document.querySelector('#add');
 const btnSubtract = document.querySelector('#subtract');
 const increAndDecre = document.querySelector('.incre-and-decre');
-
+const payment=document.querySelector('.payment-1').value;
+const price=document.querySelector('.price');
+const c=document.querySelector('.text-checkbox-5-5');
+const quantity=document.querySelector('.quantity');
+sum=0;
+let a=payment;
 btnAdd.addEventListener('click', () => {
     increAndDecre.value = parseInt(increAndDecre.value) + 1;
-    console.log(increAndDecre.value);
+    sum=a*increAndDecre.value;
+    let format=sum.toLocaleString('vi-VN',{
+        styles:'currency',
+        currency:'VND'
+    })
+    price.innerHTML=format;
+    c.innerHTML=format;
+    quantity.innerHTML=increAndDecre.value;
 });
 
 btnSubtract.addEventListener('click', () => {   
     if (increAndDecre.value > 1){
         increAndDecre.value = parseInt(increAndDecre.value) - 1;
         console.log(increAndDecre.value);
+        sum=a*increAndDecre.value;
+        let format=sum.toLocaleString('vi-VN',{
+            styles:'currency',
+            currency:'VND'
+        })
+    price.innerHTML=format;
+    c.innerHTML=format;
+    quantity.innerHTML=increAndDecre.value;
     }
 });
+
+
+
